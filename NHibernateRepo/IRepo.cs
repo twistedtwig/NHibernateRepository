b442b4464de13898b648a097880924d5471de44e
Jon Hawkins <jonathan1@houseofhawkins.com>
2015-08-18 18:21:26 +0100

    initial commit

---
+++ NHibernateRepo/IRepo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace NHibernateRepo
+{
+    public interface IRepo<TEntity, TOverride>
+        where TEntity : class
+        where TOverride : class
+    {
+        RepoTransaction<TEntity, TOverride> BeginTransaction();
+
+        T Get<T, TId>(TId id);
+
+        void Update<T>(T entity);
+        void Create<T>(T entity);
+
+        TP[] List<TE, TP>(Expression<Func<TE, bool>> exp);
+        TP Single<TE, TP>(Expression<Func<TE, bool>> exp);
+        TP SingleOrDefault<TE, TP>(Expression<Func<TE, bool>> exp);
+        TP First<TE, TP>(Expression<Func<TE, bool>> exp);
+        TP FirstOrDefault<TE, TP>(Expression<Func<TE, bool>> exp);
+    }
+
+    public interface IRepoTransaction<TEntity, TOverride> : IDisposable
+        where TEntity : class
+        where TOverride : class
+    {
+
+        void Commit();
+        void Rollback();
+    }
+}
---
+++ NHibernateRepo/Repo.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using AutoMapper.QueryableExtensions;
+using NHibernate;
+using NHibernate.Linq;
+
+namespace NHibernateRepo
+{
+    public class Repo<TEntity, TOverride>
+        : IRepo<TEntity, TOverride> 
+        where TEntity : class
+        where TOverride : class
+    {
+        private readonly string _connectionStringOrName;
+        private readonly ISessionFactory _sessionFactory;
+
+        private ISession _session;
+
+        protected ISession Session
+        {
+            get
+            {
+                if (_session == null || !_session.IsOpen)
+                {
+                    _session = _sessionFactory.OpenSession();
+                }
+
+                return _session;
+            }
+        }
+
+
+
+        public Repo(string connectionStringOrName)
+        {
+            _connectionStringOrName = connectionStringOrName;
+            var repoSetup = new RepoSetup<TEntity, TOverride>(_connectionStringOrName);
+            _sessionFactory = repoSetup.SessionFactory;
+        }
+
+        public RepoTransaction<TEntity, TOverride> BeginTransaction()
+        {
+            return new RepoTransaction<TEntity, TOverride>(_connectionStringOrName);
+        }
+
+        #region Entity methods
+
+
+        public T Get<T, TId>(TId id)
+        {
+            return Session.Load<T>(id);
+        }
+
+
+        public void Update<T>(T entity)
+        {
+            if (entity == null) throw new ArgumentNullException("entity", "Cannot update null entity");
+
+            Session.Update(entity);
+            Session.Flush();
+        }
+
+        public void Create<T>(T entity)
+        {
+            if (entity == null) throw new ArgumentNullException("entity", "Cannot create null entity");
+
+            Session.Save(entity);
+            Session.Flush();
+        }
+
+
+        #endregion
+
+
+        #region Project Queries
+
+
+        public TP[] List<TE, TP>(Expression<Func<TE, bool>> exp)
+        {
+            return Session.Query<TE>().Where(exp).Project().To<TP>().AsQueryable().ToArray();
+        }
+
+        public TP Single<TE, TP>(Expression<Func<TE, bool>> exp)
+        {
+            return Session.Query<TE>().Where(exp).ProjectTo<TP>().Single();
+        }
+
+        public TP SingleOrDefault<TE, TP>(Expression<Func<TE, bool>> exp)
+        {
+            return Session.Query<TE>().Where(exp).ProjectTo<TP>().SingleOrDefault();
+        }
+
+        public TP First<TE, TP>(Expression<Func<TE, bool>> exp)
+        {
+            return Session.Query<TE>().Where(exp).ProjectTo<TP>().First();
+        }
+
+        public TP FirstOrDefault<TE, TP>(Expression<Func<TE, bool>> exp)
+        {
+            return Session.Query<TE>().Where(exp).ProjectTo<TP>().FirstOrDefault();
+        }
+
+
+        #endregion
+    }
+
+    public class RepoTransaction<TEntity, TOverride> : Repo<TEntity, TOverride>, IRepoTransaction<TEntity, TOverride> , IDisposable
+        where TEntity : class
+        where TOverride : class
+    {
+        private ITransaction _transaction;
+
+
+        internal RepoTransaction(string connectionStringOrName)
+            : base(connectionStringOrName)
+        {
+            _transaction = Session.BeginTransaction();
+        }
+
+        public void Commit()
+        {
+            if (_transaction != null)
+            {
+                if (_transaction.IsActive)
+                {
+                    _transaction.Commit();
+                }
+            }
+        }
+
+        public void Rollback()
+        {
+            if (_transaction != null)
+            {
+                if (_transaction.IsActive)
+                {
+                    _transaction.Rollback();
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            if (_transaction != null)
+            {
+                if (_transaction.IsActive)
+                {
+                    _transaction.Rollback();
+                }
+
+                _transaction = null;
+            }
+        }
+    }
+}
